d0b2b41a280537c3665bd764ec30c382399a6543
luashiping <18069879858@163.com>
2016-03-15 15:40:41 +0800

    ping -c 3

--- lib/pub_function.py
+++ lib/pub_function.py
@@ -29,8 +29,7 @@ def get_hostadrr():
 ##ip检测
 def check_ip(ip):
 	try:
-		res = '12'
-		#res = subprocess.call(['ping', '-c', '3', ip])
+		res = subprocess.call(['ping', '-c', '3', ip])
 		if res == 0:	
 			ptred("危险提示:你所设定的虚拟机IP地址可能与现有IP地址产生冲突,请更换")
 			exit(1)
